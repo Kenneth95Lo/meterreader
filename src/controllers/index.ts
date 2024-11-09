d0999fe53831748776a9c2ec13ea3b5944fb9b52
Kenneth Lo <kenneth95lo@gmail.com>
2024-11-09 18:15:46 +0800

    Add basic implementation to Controller

---
+++ src/controllers/index.ts
@@ -0,0 +1,21 @@
+import { Nem12Parser, Nem12ParserWithFile, Nem12ParserWithRawCsv } from '../classes/nem12-parser'
+
+class MeterReadingController {
+
+    filePath: string
+
+    constructor(filePath: string){
+        this.filePath = filePath
+    }
+
+    async processMeterReadingsCsv(){
+        try{
+            const parser: Nem12Parser = new Nem12ParserWithFile(this.filePath)
+            const parsedData = await parser.parseData()
+
+            const adaptedData = parser.adaptData(parsedData)
+
+            //get the sum of 
+        }
+    }
+}
