62b25607b38095ed4696d5c4757cca93b67dea97
Kenneth Lo <kenneth95lo@gmail.com>
2024-11-10 14:15:48 +0800

    Add test for controller

---
+++ src/controllers/meter-readings-controller/__tests__/index.test.ts
@@ -0,0 +1,20 @@
+import { MeterReadingController } from "..";
+import { Nem12ParserWithFile } from "../../../classes/nem12-parser";
+import { MeterReadingModel } from "../../../model/meter-reading-model";
+
+describe("Meter Reading Controller Tests", () => {
+    it('should complete all process until DB insertion', async ()=>{
+        const bulkCreateSpy = jest.spyOn(MeterReadingModel, 'bulkCreate').mockResolvedValueOnce([])
+        const mrController = new MeterReadingController(__dirname + '/../../../__mocks__/mock-nem12.csv')
+        await mrController.processMeterReadingsCsv()
+        expect(bulkCreateSpy).toHaveBeenCalledTimes(1);
+    })
+
+    it('should fail when unable to parse data', async ()=>{
+        // const bulkCreateSpy = jest.spyOn(MeterReadingModel, 'bulkCreate').mockResolvedValueOnce([])
+        // const mrController = new MeterReadingController(__dirname + '/../../../__mocks__/mock-nem12.csv')
+        // await mrController.processMeterReadingsCsv()
+        // expect(bulkCreateSpy).toHaveBeenCalledTimes(1);
+
+    })
+})
